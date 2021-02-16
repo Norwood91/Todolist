e279769b439e99d586be8d7ac9854bf04aaef979
Robert_Norwood <robnorwood91@gmail.com>
2021-02-16 00:51:15 -0800

    User can now filter through todos.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
 
 //Functions
 const addTodo = (event) => {
@@ -43,6 +44,31 @@ const deleteTodo = (event) => {
   }
 };
 
+const filterTodo = (event) => {
+  const todos = todoList.childNodes;
+  todos.forEach(function (todo) {
+    switch (event.target.value) {
+      case 'all':
+        todo.style.display = 'flex';
+        break;
+      case 'completed':
+        if (todo.classList.contains('completed')) {
+          todo.style.display = 'flex';
+        } else {
+          todo.style.display = 'none';
+        }
+        break;
+      case 'incomplete':
+        if (!todo.classList.contains('completed')) {
+          todo.style.display = 'flex';
+        } else {
+          todo.style.display = 'none';
+        }
+        break;
+    }
+  });
+};
 //Event Listeners
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteTodo);
+filterOption.addEventListener('click', filterTodo);
